9b680f2bfd3f85ec3be0d5191101590d60d58658
Commander <root@cia.com>
2025-02-04 19:33:35 +0200

    working on fucntions

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import requests
 from settings import BaseUrl, BaseText,FakeHeaders, FailString
 
-print(BaseUrl)
+
+
+def ReadTxtFile(FileName):
+    TxtFile = open(FileName, "r")
+    TxtFileData = TxtFile.read()
+    TxtFileList = TxtFileData.split("\n")
+    TxtFile.close()
+
+ReadTxtFile("fuzzcoupons.txt")
 
 
 
-for i in BaseText:
-    TestCuponUrl = BaseUrl+i
-    PossibleRequest = requests.get(TestCuponUrl, headers=FakeHeaders)
-    print(PossibleRequest.status_code)
-    print(PossibleRequest.text)
-    if PossibleRequest.text == FailString:
-        print("lol")
-    else:
-        print("very good")
+# for i in BaseText:
+#     TestCuponUrl = BaseUrl+i
+#     PossibleRequest = requests.get(TestCuponUrl, headers=FakeHeaders)
+#     print(PossibleRequest.status_code)
+#     print(PossibleRequest.text)
+#     if PossibleRequest.text == FailString:
+#         print("lol")
+#     else:
+#         print("very good")
