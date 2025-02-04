79db7b6e9d3a9272aaed89e38b411a64e7f184bb
Commander <root@cia.com>
2025-02-04 19:24:40 +0200

    is it working?

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
-from settings import BaseUrl, BaseText
+from settings import BaseUrl, BaseText,FakeHeaders, FailString
 
 print(BaseUrl)
 
 
+
 for i in BaseText:
     TestCuponUrl = BaseUrl+i
-    PossibleRequest = requests.get(TestCuponUrl)
+    PossibleRequest = requests.get(TestCuponUrl, headers=FakeHeaders)
     print(PossibleRequest.status_code)
     print(PossibleRequest.text)
+    if PossibleRequest.text == FailString:
+        print("lol")
+    else:
+        print("very good")
--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # https://www.shirion-computers.com/services/CartServices.asp?service=setcoupon&CouponCode=NOD3ftp
 
 BaseUrl = "https://www.shirion-computers.com/services/CartServices.asp?service=setcoupon&CouponCode="
-
+FakeHeaders = {"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'"}
 
 BaseText = [
     "TEST",
