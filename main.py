5840fe0db5da60ef1d949fa78ed9e6d6643374d0
Commander <root@cia.com>
2025-02-04 19:21:35 +0200

    Your request triggered an alert! If you feel that you have received this page in error, please contact the administrator of this web site.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-
-from settings import BaseUrl
+import requests
+from settings import BaseUrl, BaseText
 
 print(BaseUrl)
+
+
+for i in BaseText:
+    TestCuponUrl = BaseUrl+i
+    PossibleRequest = requests.get(TestCuponUrl)
+    print(PossibleRequest.status_code)
+    print(PossibleRequest.text)
--- settings.py
+++ settings.py
@@ -1,3 +1,16 @@
 # https://www.shirion-computers.com/services/CartServices.asp?service=setcoupon&CouponCode=NOD3ftp
 
 BaseUrl = "https://www.shirion-computers.com/services/CartServices.asp?service=setcoupon&CouponCode="
+
+
+BaseText = [
+    "TEST",
+    "SAVE",
+    "FALL",
+    "SUMMER",
+    "SPRING",
+    "WINTER",
+    "Welcome",
+]
+
+FailString = "0|לא נמצא קופון לקוד זה"
