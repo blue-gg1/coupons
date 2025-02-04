526d8f081bfe83f52efe0ca3883a3c635cf3fca0
Commander <root@cia.com>
2025-02-04 19:40:06 +0200

    testing the logging

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import logging
 from settings import BaseUrl, BaseText,FakeHeaders, FailString
 
 
@@ -21,17 +22,19 @@ def ReadTxtFile(FileName):
 #             print("very good")
 #     pass
 
-# for i in BaseText:
-#     TestCuponUrl = BaseUrl+i
-#     PossibleRequest = requests.get(TestCuponUrl, headers=FakeHeaders)
-#     print(PossibleRequest.status_code)
-#     print(PossibleRequest.text)
-#     if PossibleRequest.text == FailString:
-#         print("lol")
-#     else:
-#         print("very good")
+for i in BaseText:
+    TestCuponUrl = BaseUrl+i
+    PossibleRequest = requests.get(TestCuponUrl, headers=FakeHeaders)
+    print(PossibleRequest.status_code)
+    print(PossibleRequest.text)
+    if PossibleRequest.text == FailString:
+        print("lol")
+        logging.info("\r\nDud" + i + PossibleRequest.url)
+    else:
+        print("very good")
+        logging.info("\r\nWe got one!, code is" + i + PossibleRequest.url)
 
 
 ReadTxtFile("fuzzcoupons.txt")
-UseListGetCupon(TxtFileList)
+# UseListGetCupon(TxtFileList)
 
