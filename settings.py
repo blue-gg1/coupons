fbed1e0cd5a3a57f38eabce89979743147a9173b
Commander <root@cia.com>
2025-02-04 19:14:45 +0200

    added the start

---
+++ settings.py
@@ -0,0 +1,3 @@
+# https://www.shirion-computers.com/services/CartServices.asp?service=setcoupon&CouponCode=NOD3ftp
+
+BaseUrl = "https://www.shirion-computers.com/services/CartServices.asp?service=setcoupon&CouponCode="
